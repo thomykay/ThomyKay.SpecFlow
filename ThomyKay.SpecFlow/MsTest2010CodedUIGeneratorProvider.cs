ac2dc614bc5cb015b35fcec5ea5dbb8f87e579a3
Thomy Kay <thomy.kay@online.de>
2011-04-20 11:07:29 +0200

    Initial version

---
+++ ThomyKay.SpecFlow/MsTest2010CodedUIGeneratorProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow.Generator.UnitTestProvider;
+using System.CodeDom;
+
+namespace Agilent.SpecFlow
+{
+    public class MsTest2010CodedUIGeneratorProvider : MsTest2010GeneratorProvider
+    {
+        public override void SetTestFixture(System.CodeDom.CodeTypeDeclaration typeDeclaration, string title, string description)
+        {
+            base.SetTestFixture(typeDeclaration, title, description);
+            foreach (CodeAttributeDeclaration customAttrbute in typeDeclaration.CustomAttributes)
+            {
+                if (customAttrbute.Name == "Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute")
+                {
+                    typeDeclaration.CustomAttributes.Remove(customAttrbute);
+                    break;
+                }
+            }
+
+            typeDeclaration.CustomAttributes.Add(new CodeAttributeDeclaration(new CodeTypeReference("Microsoft.VisualStudio.TestTools.UITesting.CodedUITestAttribute")));
+        }
+    }
+}
